c2eca6da92ef8a487095a02a21ec06c8858a7646
mateokilla <nitro_5_bernath_mark@outlook.hu>
2023-11-04 13:55:12 +0100

    szelekcio

--- objektumok.py
+++ objektumok.py
@@ -6,3 +6,10 @@ class Szemely:
 szemely1 = Szemely("Mark", 30)
 print ("Hello " + szemely1.nev, szemely1.kor)
        
+#szelekcio
+def ParosE(szam):
+    if szam%2==0:
+        print("A szam paros")
+    else:
+        print("A szam paratlan.")
+print(ParosE(3))
